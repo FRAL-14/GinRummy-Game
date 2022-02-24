82286971d9ecfb159f6e94f945a159608f5486fe
noura.mohamed@student.kdg.be <noura.mohamed@student.kdg.be>
2022-02-24 21:50:44 +0100

    Hand class constructor gives a player 10 cards

--- src/be/kdg/ginrummy/Hand.java
+++ src/be/kdg/ginrummy/Hand.java
@@ -1,10 +1,22 @@
 package be.kdg.ginrummy;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Hand extends Deck{
+public class Hand extends Deck {
     protected List<Card> updatedCardList;
     protected List<Card> playerCards;
-    private int deadWoodCount=0;
+    private int deadWoodCount = 0;
     private List<Card> meld;
+
+    public Hand(Deck deck) {
+        updatedCardList = new ArrayList<>(deck.getDeckOfCards());
+        playerCards = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            playerCards.add(deck.getCardAt(i));
+            updatedCardList.remove(0);
+        }
+        deck.setDeckOfCards(updatedCardList);
+    }
+
 }
