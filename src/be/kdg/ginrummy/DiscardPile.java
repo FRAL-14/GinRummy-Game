11e8ccc1ff439a14f8f04b0a2d8b6145a88f409c
noura.mohamed@student.kdg.be <noura.mohamed@student.kdg.be>
2022-02-25 02:01:09 +0100

    Added methods to DiscardPile class

--- src/be/kdg/ginrummy/DiscardPile.java
+++ src/be/kdg/ginrummy/DiscardPile.java
@@ -1,9 +1,28 @@
 package be.kdg.ginrummy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DiscardPile {
     private List<Card> discardedCards;
     private int size= 0;
+
+    public DiscardPile(){
+        discardedCards = new ArrayList<>();
+    }
+    protected void discardCard(Card card){
+        discardedCards.add(card);
+    }
+    protected void drawCardFor(Player player){
+        size = discardedCards.size();
+        player.addCard(discardedCards.get(size-1));
+        discardedCards.remove(size-1);
+    }
+    protected void printDiscardPile(){
+        for (Card card: discardedCards){
+            System.out.print(card +" - ");
+        }
+        System.out.println();
+    }
 
 }
