05895843e94f4d5fa4b234c8029025d94180180b
Elina Van der Taelen <elina.vandertaelen@student.kdg.be>
2022-02-25 14:41:48 +0100

    changed deck attribute to LinkedList

--- src/be/kdg/ginrummy/Deck.java
+++ src/be/kdg/ginrummy/Deck.java
@@ -1,32 +1,40 @@
 package be.kdg.ginrummy;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Deck {
-    private final int totalCardsNumber = 52;
     private List<Card> deck;
 
-    public Deck(){
-        deck = new ArrayList<>(totalCardsNumber);
-        int index=0;
-        for (int suit = 0; suit <Card.Suit.values().length; suit++){
-            for(int rank = 0; rank <Card.Rank.values().length; rank++){
+    public Deck() {
+        deck = new LinkedList<>();
+        for (int suit = 0; suit < Card.Suit.values().length; suit++) {
+            for (int rank = 0; rank < Card.Rank.values().length; rank++) {
                 deck.add(new Card(Card.Rank.values()[rank], Card.Suit.values()[suit]));
-                index++;
             }
         }
-        // Collections.shuffle(deckOfCards);
+        // Collections.shuffle(deck);
     }
 
-    protected void print(){
-        for (Card card: deck){ System.out.println(card);}
+    protected void print() {
+        for (Card card : deck) {
+            System.out.println(card);
+        }
     }
 
-    protected void setDeckOfCards(List<Card> updatedDeckOfCards) { this.deck = new ArrayList<>(updatedDeckOfCards);}
-    public  List<Card> getDeckOfCards() {
+    public List<Card> getDeckOfCards() {
         return deck;
     }
-    protected Card getCardAt(int i){return deck.get(i);}
-    protected void removeCard(int i){deck.remove(i);}
+
+    protected void setDeckOfCards(List<Card> updatedDeckOfCards) {
+        this.deck = new LinkedList<>(updatedDeckOfCards);
+    }
+
+    protected Card getCardAt(int i) {
+        return deck.get(i);
+    }
+
+    protected void removeCard(int i) {
+        deck.remove(i);
+    }
 }
