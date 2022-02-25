6b9981992c31800ce22a5406b09f055f7e596ce0
Noura Mohamed <noura.mohamed@student.kdg.be>
2022-02-25 00:12:04 +0000

    Revert "Merge branch 'cards' into 'main'"
    This reverts merge request !1

--- src/be/kdg/ginrummy/DiscardPile.java
+++
@@ -1,9 +0,0 @@
-package be.kdg.ginrummy;
-
-import java.util.List;
-
-public class DiscardPile {
-    private List<Card> discardedCards;
-    private int size= 0;
-
-}
--- src/be/kdg/ginrummy/Runner.java
+++ src/be/kdg/ginrummy/Runner.java
@@ -5,18 +5,7 @@ public class Runner {
     private static Game game;
 
     public static void main(String[] args) {
-
         game = new Game("Leo");
-
-        //testing
-        Deck deck = new Deck();
-        //deck.print();
-
-        //testing
-        Hand playerOne = new Hand(deck);
-        playerOne.printPlayerCards();
-        //testing after player draw 10 cards
-        //deck.print();
     }
 
     public static Game getGame() {
