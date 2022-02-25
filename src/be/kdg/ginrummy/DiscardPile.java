b0bddc3c395fc9a6ced758695f4b3e1cab76ef18
Elina Van der Taelen <elina.vandertaelen@student.kdg.be>
2022-02-25 16:37:47 +0100

    made discardPile a LinkedList

--- src/be/kdg/ginrummy/DiscardPile.java
+++ src/be/kdg/ginrummy/DiscardPile.java
@@ -1,26 +1,29 @@
 package be.kdg.ginrummy;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DiscardPile {
     private List<Card> discardedCards;
-    private int size= 0;
+    private int size = 0;
 
-    public DiscardPile(){
-        discardedCards = new ArrayList<>();
+    public DiscardPile() {
+        discardedCards = new LinkedList<>();
     }
-    protected void discardCard(Card card){
+
+    protected void discardCard(Card card) {
         discardedCards.add(card);
     }
-    protected void drawCardFor(Player player){
+
+    protected void drawCardFor(Player player) {
         size = discardedCards.size();
-        player.addCard(discardedCards.get(size-1));
-        discardedCards.remove(size-1);
+        player.addCard(discardedCards.get(size - 1));
+        discardedCards.remove(size - 1);
     }
-    protected void printDiscardPile(){
-        for (Card card: discardedCards){
-            System.out.print(card +" - ");
+
+    protected void printDiscardPile() {
+        for (Card card : discardedCards) {
+            System.out.print(card + " - ");
         }
         System.out.println();
     }
