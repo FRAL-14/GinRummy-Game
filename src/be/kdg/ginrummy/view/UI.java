70276ad74a1a2b425fab6294a6a7ea1fdcc7ae6f
Elina Van der Taelen <elina.vandertaelen@student.kdg.be>
2022-03-09 14:02:53 +0100

    changed scanner attribute to final in UI class, added comments

--- src/be/kdg/ginrummy/view/UI.java
+++ src/be/kdg/ginrummy/view/UI.java
@@ -5,13 +5,22 @@ import java.util.Scanner;
 
 public class UI {
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in); // Can be final since the scanner doesn't change
 
+    /**
+     * Asks for basic input.
+     * @return The input in lowercase.
+     */
     static String askUserInput() {
         System.out.print(" > ");
-        return scanner.nextLine().toLowerCase(Locale.ROOT);
+        return SCANNER.nextLine().toLowerCase(Locale.ROOT);
     }
 
+    /**
+     * Called by Game.regularGameChecks() when a user should be able to knock.
+     * The user is asked if they want to knock.
+     * @return Whether the player wants to knock.
+     */
     static boolean askUserIfKnock() {
         System.out.println("Do you want to knock? (y/n)");
         String input;
@@ -21,7 +30,7 @@ public class UI {
             if (!"y".equals(input) && !"n".equals(input)) {
                 System.out.println("Sorry, incorrect input.");
             }
-        } while (!"y".equals(input) && !"n".equals(input));
+        } while (!"y".equals(input) && !"n".equals(input)); // Checking for correct input, if not, ask again
 
         return "y".equals(input);
     }
