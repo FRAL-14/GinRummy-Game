984ec2adb23be18cf2570198ab4df33cba6d3769
noura.mohamed@student.kdg.be <noura.mohamed@student.kdg.be>
2022-03-21 12:51:02 +0100

    commit

--- src/be/kdg/ginrummy/Runner.java
+++ src/be/kdg/ginrummy/Runner.java
@@ -18,11 +18,11 @@ public class Runner extends Application {
 //        //testing
 //        Deck deck = new Deck();
 //        //deck.print(); full 52 cards set
-//
+
 //        Player playerOne = new Player("Bear",0,false);
 //        playerOne.dealCards(deck);
 //        //playerOne.printCards();
-//
+
 //        Player playerTwo = new Player("Bear2",0,false);
 //        playerTwo.dealCards(deck);
 //        playerTwo.printCards();
