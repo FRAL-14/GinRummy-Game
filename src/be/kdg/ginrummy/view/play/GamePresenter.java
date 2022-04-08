eb8c2babfc6a10660e84e1871de12be6d1ba42f4
noura.mohamed@student.kdg.be <noura.mohamed@student.kdg.be>
2022-04-08 02:41:56 +0200

    Created game presenter class

--- src/be/kdg/ginrummy/view/play/GamePresenter.java
+++ src/be/kdg/ginrummy/view/play/GamePresenter.java
@@ -1,2 +1,24 @@
-package be.kdg.ginrummy.view.play;public class GamePresenter {
+package be.kdg.ginrummy.view.play;
+
+import be.kdg.ginrummy.model.Game;
+
+
+public class GamePresenter {
+    private Game model;
+    private GameView view;
+
+    public GamePresenter(Game model, GameView view) {
+        this.model = model;
+        this.view=view;
+        addEventHandlers();
+        updateView();
+    }
+
+    private void addEventHandlers(){
+
+    }
+    private void updateView(){
+        // fills the view with model data
+
+    }
 }
