ea4d03660cdcb4cfb7f02bdc324fe7dd00893ead
noura.mohamed@student.kdg.be <noura.mohamed@student.kdg.be>
2022-04-08 02:51:34 +0200

    added eventhandlers to the play button of the home screen to display input screen

--- src/be/kdg/ginrummy/view/home/HomePresenter.java
+++ src/be/kdg/ginrummy/view/home/HomePresenter.java
@@ -1,11 +1,15 @@
 package be.kdg.ginrummy.view.home;
 
 import be.kdg.ginrummy.model.Game;
+import be.kdg.ginrummy.model.Player;
+import be.kdg.ginrummy.view.name.NamePresenter;
+import be.kdg.ginrummy.view.name.NameView;
 import javafx.stage.Window;
 
 public class HomePresenter {
 
     private final Game MODEL;
+    private Player playerModel;
     private final HomeView VIEW;
 
 
@@ -21,6 +25,10 @@ public class HomePresenter {
     private void addEventHandlers() {
         // add event handlers to view controls (lambdas)
         // in the event handlers: call model methods and updateView()
+        this.VIEW.getPlayButton().setOnAction(e->
+        {
+            nameInput();
+        });
     }
 
     private void updateView() {
@@ -30,6 +38,12 @@ public class HomePresenter {
     public void addWindowEventHandlers() {
         Window window = VIEW.getScene().getWindow();
         // add event handlers to window
+    }
+    //method for transfering to name input screen
+    private void nameInput(){
+        NameView nameView = new NameView();
+        NamePresenter namePresenter = new NamePresenter(playerModel, nameView);
+        VIEW.getScene().setRoot(nameView);
     }
 
 }
