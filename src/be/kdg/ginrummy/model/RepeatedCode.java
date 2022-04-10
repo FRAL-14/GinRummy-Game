f0d7ffa2a7ef5ddf1d3a95d075e2f9ec6c8b36f0
noura.mohamed@student.kdg.be <noura.mohamed@student.kdg.be>
2022-04-10 04:04:35 +0200

    Trying to collect all repeated method in one class

---
+++ src/be/kdg/ginrummy/model/RepeatedCode.java
@@ -0,0 +1,19 @@
+package be.kdg.ginrummy.model;
+
+import be.kdg.ginrummy.view.rules.RulesView;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class RepeatedCode {
+    public static void viewRules(Scene scene) {
+        RulesView rulesView = new RulesView();
+        Stage helpStage = new Stage();
+        helpStage.initOwner(scene.getWindow());
+        helpStage.initModality(Modality.APPLICATION_MODAL);
+        helpStage.setScene(new Scene(rulesView));
+        helpStage.setX(scene.getWindow().getX());
+        helpStage.setY(scene.getWindow().getY());
+        helpStage.showAndWait();
+    }
+}
