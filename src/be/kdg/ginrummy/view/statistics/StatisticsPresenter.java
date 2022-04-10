7a2dac92ea12839bcd7e4e2780eab3efa7b56340
noura.mohamed@student.kdg.be <noura.mohamed@student.kdg.be>
2022-04-10 05:20:51 +0200

    added menu bar to statistics screen

--- src/be/kdg/ginrummy/view/statistics/StatisticsPresenter.java
+++ src/be/kdg/ginrummy/view/statistics/StatisticsPresenter.java
@@ -2,12 +2,16 @@ package be.kdg.ginrummy.view.statistics;
 
 import be.kdg.ginrummy.model.Game;
 import be.kdg.ginrummy.model.GameStatistics;
+import be.kdg.ginrummy.model.ViewRules;
+import be.kdg.ginrummy.view.home.HomePresenter;
+import be.kdg.ginrummy.view.home.HomeView;
 
 public class StatisticsPresenter {
     //private attributes
 
     private GameStatistics model;
     private StatisticsView view;
+    private Game modelGame;
 
     public StatisticsPresenter(GameStatistics model, StatisticsView view) {
         this.model = model;
@@ -16,7 +20,25 @@ public class StatisticsPresenter {
         updateView();
 
     }
-    private void addEventHandlers(){}
+    private void addEventHandlers(){
+        view.getHomeMenuItem().setOnAction(e->{
+            returnToHomeScreen();
+        });
+        view.getRulesMenuItem().setOnAction(e->{
+            displayRulesScreen();
+        });
+    }
     private void updateView(){}
     private void addWindowEventHandlers(){}
+
+    //methods for event handlers
+    private void returnToHomeScreen(){
+        HomeView homeView = new HomeView();
+        HomePresenter homePresenter = new HomePresenter(modelGame, homeView);
+        view.getScene().setRoot(homeView);
+    }
+    private void displayRulesScreen(){
+        ViewRules.viewRules(view.getScene());
+    }
+
 }
--- src/be/kdg/ginrummy/view/statistics/StatisticsView.java
+++ src/be/kdg/ginrummy/view/statistics/StatisticsView.java
@@ -1,14 +1,53 @@
 package be.kdg.ginrummy.view.statistics;
 
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
-public class StatisticsView extends BorderPane {
+public class StatisticsView extends VBox {
+    //private menu Bar attributes
+    private MenuBar menuBar;
+    private Menu menu;
+    private MenuItem home;
+    private MenuItem rules;
+    private Label label;
 
     public StatisticsView() {
         initialiseNodes();
         layoutNodes();
     }
 
-    private void initialiseNodes(){}
-    private void layoutNodes(){}
+    private void initialiseNodes(){
+        //menu Bar attributes
+        menuBar = new MenuBar();
+        menu = new Menu("Menu");
+
+        home = new MenuItem("Home");
+        home.setGraphic(new ImageView(new Image("/icons/home.png", 16, 16, false, false)));
+
+        rules = new MenuItem("Rules");
+        rules.setGraphic(new ImageView(new Image("/icons/rules.png", 16, 16, false, false)));
+
+        label = new Label("this screen is for all games statistics");
+    }
+    private void layoutNodes(){
+        //menu Bar
+        menu.getItems().add(home);
+        menu.getItems().add(rules);
+        menuBar.getMenus().add(menu);
+        getChildren().add(menuBar);
+        getChildren().add(label);
+        setSpacing(50);
+    }
+
+    public MenuItem getHomeMenuItem() {
+        return home;
+    }
+    public MenuItem getRulesMenuItem() {
+        return rules;
+    }
 }
