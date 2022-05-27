0fd9a939170d375b3958a55d992533ae72837a7b
Noura <noura.mohamed@student.kdg.be>
2022-05-27 18:05:14 +0200

    Fixed possible errors in DBConnection class

--- ginrummy/src/model/DB_Connection.java
+++ ginrummy/src/model/DB_Connection.java
@@ -12,11 +12,13 @@ public class DB_Connection{
 
     public static Connection connectToDB(){
         try {
-            Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/javafx_game", "postgres", "Student_1234");
+            Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/javafx_game",
+                    "postgres", "Student_1234");
             System.out.println("Connected to the PostgreSQL server successfully.");
             return c;
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println("Failed to connect to the database");
             return null;
         }
     }
