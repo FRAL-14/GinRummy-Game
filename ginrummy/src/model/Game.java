3f94602b4d4bb570cf1ce4c8d34e2fc23671d805
Noura <noura.mohamed@student.kdg.be>
2022-05-27 18:07:02 +0200

    added more attributes in game class for statistics

--- ginrummy/src/model/Game.java
+++ ginrummy/src/model/Game.java
@@ -1,29 +1,49 @@
 package model;
 
-import view.UI;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+
 
 public class Game {
 
 	private static final int POINT_THRESHOLD = 100;
 
-	private final Player HUMAN_PLAYER; // Can be final, as only 1 human player is needed for a game and doesn't change
-	private final Player COMPUTER_PLAYER; // Can be final, as only 1 CPU player is needed for a game and doesn't change
-	private final DiscardPile DISCARD_PILE = new DiscardPile(); // Can be final, as only 1 is needed and doesn't change, its cards can be changed
-	private final Deck DECK = new Deck(); // Can be final, as only 1 deck is needed for a game and doesn't change
+	private final Player HUMAN_PLAYER;
+	private final Player COMPUTER_PLAYER;
+	private final DiscardPile DISCARD_PILE = new DiscardPile();
+	private final Deck DECK = new Deck();
 	private int turnNumber;
 	private long startingTime;
+
+	//for a single game
+	private Timestamp gameStartTime;
+	private Timestamp gameEndTime;
+	private int gameDurationInMs; //In milliseconds
+
+	//for human turns only
+	private int humanTurn=0;
+	private int turnDurationInMs; //In milliseconds
+	private Timestamp humanTurnStarts =Timestamp.from(Instant.now());
+	private Timestamp humanTurnEnds;
+
+
 
 
 	public Game() {
 		this.turnNumber = 0;
 		setStartingTimeToNow();
 
-		// Making the players
-		this.HUMAN_PLAYER = new Player(0, true);
-		this.COMPUTER_PLAYER = new Player("Computer", 0, false);
+		this.HUMAN_PLAYER = new Player(true);
+		this.COMPUTER_PLAYER = new Player("Computer", false);
 
+		DB_Connection.connectToDB();
+		GameStatistics.createTables();
 		dealCards();
 	}
+
+
 
 
 	public void dealCards() {
@@ -32,61 +52,8 @@ public class Game {
 		DISCARD_PILE.addCard(DECK.getNextCard());
 	}
 
-	public int getTurnNumber() {
-		return turnNumber;
-	}
-
-	public void incrementTurnNumber() {
-		turnNumber++;
-	}
-
-	public DiscardPile getDISCARD_PILE() {
-		return DISCARD_PILE;
-	}
-
-	public Deck getDECK() {
-		return DECK;
-	}
-
-
-	public long getStartingTime() {
-		return startingTime;
-	}
-
-	public void setStartingTimeToNow() {
-		this.startingTime = System.currentTimeMillis();
-	}
-
-	public Player getHUMAN_PLAYER() {
-		return HUMAN_PLAYER;
-	}
-
-	public Player getCOMPUTER_PLAYER() {
-		return COMPUTER_PLAYER;
-	}
-
-	public boolean humanIsPlaying() {
-		return HUMAN_PLAYER.getIsPlayerTurn();
-	}
-
-	/**
-	 * Sorts the player's hand and checks if a user can knock.
-	 * Should be run after every move.
-	 */
-	public void regularGameChecks() {
-		getHUMAN_PLAYER().getHAND().sortPlayerCards();
-
-		if (getHUMAN_PLAYER().canKnock()) {
-			if (UI.askUserIfKnock()) {
-				DISCARD_PILE.addCard(getHUMAN_PLAYER().getHAND().removeLastCard());
-				distributePoints();
-			}
-		}
-	}
 
 	public void switchTurn() {
-		// TODO: create new Session?
-
 		if (getHUMAN_PLAYER().getIsPlayerTurn()) {
 			getHUMAN_PLAYER().setIsPlayerTurn(false);
 			getCOMPUTER_PLAYER().setIsPlayerTurn(true);
@@ -94,10 +61,15 @@ public class Game {
 			getHUMAN_PLAYER().setIsPlayerTurn(true);
 			getCOMPUTER_PLAYER().setIsPlayerTurn(false);
 		}
+
+
 	}
+
+
 
 	public boolean isEndOfGame() {
 		return HUMAN_PLAYER.getScore() >= POINT_THRESHOLD || COMPUTER_PLAYER.getScore() >= POINT_THRESHOLD;
+
 	}
 
 	public void distributePoints() {
@@ -115,6 +87,7 @@ public class Game {
 			newPoints += computerDeadwood - humanDeadwood;
 			getHUMAN_PLAYER().addToScore(newPoints);
 		}
+		System.out.println(HUMAN_PLAYER.getNAME()+" has "+HUMAN_PLAYER.getScore()+" points ");
 	}
 
 	public Player getPlayerWhoWon() {
@@ -131,12 +104,84 @@ public class Game {
 	}
 
 	public void startNewRound() {
-		this.turnNumber = 0;
-		setStartingTimeToNow();
-
 		DISCARD_PILE.clear();
 		DECK.resetDeck();
 		dealCards();
 	}
+
+	public int calculateTurnDuration() {
+		this.turnDurationInMs = (int) (humanTurnEnds.getTime()-humanTurnStarts.getTime());
+		return turnDurationInMs;
+	}
+
+	public Timestamp getGameStartTime() { return gameStartTime; }
+	public void setGameStartTime() { //checked
+		this.gameStartTime = Timestamp.from(Instant.now());
+		System.out.println("Game started at: "+ gameStartTime); //for checking
+	}
+
+	public Timestamp getGameEndTime() { return gameEndTime;}
+	public void setGameEndTime() { this.gameEndTime = Timestamp.from(Instant.now());}
+
+	public int getGameDurationInMs() {
+		return (int) (gameEndTime.getTime() - gameStartTime.getTime());
+	}
+
+	public Timestamp getHumanTurnStarts() {
+		return humanTurnStarts;
+	}
+
+	public int getTurnDurationInMs() {
+		return turnDurationInMs;
+	}
+
+	public Timestamp getHumanTurnEnds() {
+		return humanTurnEnds;
+	}
+
+	public int getTurnNumber() {
+		return turnNumber;
+	}
+	public void incrementTurnNumber() {
+		turnNumber++;
+	}
+	public DiscardPile getDISCARD_PILE() {
+		return DISCARD_PILE;
+	}
+	public Deck getDECK() {
+		return DECK;
+	}
+
+
+	//for timer on the game screen.
+	public long getStartingTime() {return startingTime;}
+	public void setStartingTimeToNow() { this.startingTime = System.currentTimeMillis();}
+
+	public Player getHUMAN_PLAYER() {
+		return HUMAN_PLAYER;
+	}
+	public Player getCOMPUTER_PLAYER() {
+		return COMPUTER_PLAYER;
+	}
+
+	public boolean humanIsPlaying() {
+		return HUMAN_PLAYER.getIsPlayerTurn();
+	}
+
+
+	public void setHumanTurnEnds() {
+		this.humanTurnEnds = Timestamp.from(Instant.now());
+	}
+	public void setHumanTurnStarts() {
+		this.humanTurnStarts = Timestamp.from(Instant.now());
+	}
+	public void incrementHumanTurn(){
+		if (humanIsPlaying()){
+			humanTurn+=1;
+		}
+
+	}
+
+
 
 }
